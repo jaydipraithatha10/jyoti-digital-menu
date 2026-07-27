9fd2bc2be02992f3efeb66e9859f4d8b97526739
jaydipraithatha10 <jaydipraithatha10@gmail.com>
2026-07-27 09:49:03 +0530

    Update script.js

--- script.js
+++ script.js
@@ -562,6 +562,18 @@ function searchProducts() {
 
     });
 
+// SubCategory Search
+document.querySelectorAll(".subcategory-card").forEach(sub => {
+
+    const name = sub.dataset.name || "";
+
+    if (keyword === "" || name.includes(keyword)) {
+        sub.style.display = "";
+    } else {
+        sub.style.display = "none";
+    }
+
+});
 
     // Product Search (જે ખુલેલા Products છે)
     document.querySelectorAll(".product-card").forEach(card => {
