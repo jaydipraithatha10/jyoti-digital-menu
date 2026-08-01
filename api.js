ed3dab55ba142895df44a14db3683dfd8a960736
jaydipraithatha10 <jaydipraithatha10@gmail.com>
2026-08-02 04:51:05 +0530

    Update api.js

--- api.js
+++ api.js
@@ -180,3 +180,112 @@ onerror="this.src='placeholder.png'">
     }
 
 }
+// ===========================================
+// PART 2A
+// LOAD PRODUCTS + SEARCH
+// ===========================================
+
+async function loadProducts(){
+
+    const list = document.getElementById("productList");
+
+    if(!list) return;
+
+    const subId = getParam("sub");
+    const categoryId = getParam("category");
+
+    const search =
+    (getParam("search") || "").toLowerCase();
+
+    const csv = await fetchCSV(productURL);
+
+    const rows = csvToArray(csv);
+
+    list.innerHTML = "";
+
+    rows.slice(1).forEach(row=>{
+
+        const id = row[0];
+        const catId = row[1];
+        const subCatId = row[2];
+        const product = row[3];
+        const weight = row[4];
+        const price = Number(row[5]);
+        const status = row[6];
+        const image = row[7];
+
+        if(status.trim().toLowerCase()!="active")
+            return;
+
+        // Category Filter
+
+        if(subId){
+
+            if(subCatId!=subId)
+                return;
+
+        }else if(categoryId){
+
+            if(catId!=categoryId)
+                return;
+
+        }
+
+        // Home Search
+
+        if(search){
+
+            const keyword =
+            (product + " " + weight)
+            .toLowerCase();
+
+            if(!keyword.includes(search))
+                return;
+
+        }
+
+        const item =
+        cart.find(p=>p.id==id);
+
+        const qty =
+        item ? item.qty : 0;
+
+        list.innerHTML += `
+
+<div class="product-card">
+
+<img src="${image}"
+loading="lazy"
+onerror="this.src='placeholder.png'">
+
+<h3 class="product-name">
+
+${product}
+
+</h3>
+
+<p class="product-weight">
+
+${weight}
+
+</p>
+
+<h4 class="product-price">
+
+₹${price}
+
+</h4>
+
+<div id="cart-${id}">
+
+<!-- Quantity Button Here -->
+
+</div>
+
+</div>
+
+`;
+
+    });
+
+}
